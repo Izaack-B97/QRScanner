14f8a342a6d58d3f39cd6610d3cf38b62f15487d
Isaac Bustamante <izaack.97@gmail.com>
2020-07-16 14:25:10 -0700

    Ya se cargan las ubicaciones en el historial

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public dataLocal: DataLocalService) {}
 
+  enviarCorreo(){
+    console.log('Enviando a correo');
+  }
+
+  abrirRegistro(){
+    console.log('Abriendo registro');
+  }
 }
